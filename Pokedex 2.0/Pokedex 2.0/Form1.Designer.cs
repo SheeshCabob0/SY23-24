bd5905848b9b427dccc7ae4789e41948b1f49179
Derek W <dmwhi413@learner.dvusd.org>
2024-01-23 09:34:09 -0700

    Pokedex Update 1/23/24
    Caught up to everyone else, fully saves and reads the file.

--- Pokedex 2.0/Pokedex 2.0/Form1.Designer.cs
+++ Pokedex 2.0/Pokedex 2.0/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Pokedex_2._0
+﻿namespace Pokedex
 {
     partial class Form1
     {
@@ -42,23 +42,23 @@
             this.levelTbox = new System.Windows.Forms.TextBox();
             this.shinyCbox = new System.Windows.Forms.CheckBox();
             this.legendaryCbox = new System.Windows.Forms.CheckBox();
-            this.hpUpDown = new System.Windows.Forms.NumericUpDown();
-            this.expUpDown = new System.Windows.Forms.NumericUpDown();
             this.SaveBtn = new System.Windows.Forms.Button();
             this.genUpDown = new System.Windows.Forms.NumericUpDown();
+            this.hpTbox = new System.Windows.Forms.TextBox();
+            this.expTbox = new System.Windows.Forms.TextBox();
+            this.attackComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imagePbox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.hpUpDown)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.expUpDown)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.genUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Pokedex_2._0.Properties.Resources.pokedex_3;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(34, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1021, 518);
+            this.pictureBox1.Size = new System.Drawing.Size(1132, 607);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -66,7 +66,7 @@
             // nameLbl
             // 
             this.nameLbl.AutoSize = true;
-            this.nameLbl.Location = new System.Drawing.Point(588, 174);
+            this.nameLbl.Location = new System.Drawing.Point(663, 199);
             this.nameLbl.Name = "nameLbl";
             this.nameLbl.Size = new System.Drawing.Size(44, 16);
             this.nameLbl.TabIndex = 1;
@@ -75,7 +75,7 @@
             // typeLbl
             // 
             this.typeLbl.AutoSize = true;
-            this.typeLbl.Location = new System.Drawing.Point(588, 193);
+            this.typeLbl.Location = new System.Drawing.Point(668, 224);
             this.typeLbl.Name = "typeLbl";
             this.typeLbl.Size = new System.Drawing.Size(39, 16);
             this.typeLbl.TabIndex = 2;
@@ -84,7 +84,7 @@
             // levelLbl
             // 
             this.levelLbl.AutoSize = true;
-            this.levelLbl.Location = new System.Drawing.Point(588, 213);
+            this.levelLbl.Location = new System.Drawing.Point(667, 250);
             this.levelLbl.Name = "levelLbl";
             this.levelLbl.Size = new System.Drawing.Size(40, 16);
             this.levelLbl.TabIndex = 3;
@@ -94,9 +94,9 @@
             // 
             this.imagePbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.imagePbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.imagePbox.Location = new System.Drawing.Point(220, 174);
+            this.imagePbox.Location = new System.Drawing.Point(272, 199);
             this.imagePbox.Name = "imagePbox";
-            this.imagePbox.Size = new System.Drawing.Size(232, 144);
+            this.imagePbox.Size = new System.Drawing.Size(248, 170);
             this.imagePbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imagePbox.TabIndex = 4;
             this.imagePbox.TabStop = false;
@@ -104,7 +104,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(721, 429);
+            this.label4.Location = new System.Drawing.Point(823, 503);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(33, 16);
             this.label4.TabIndex = 7;
@@ -113,7 +113,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(582, 429);
+            this.label5.Location = new System.Drawing.Point(663, 503);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(26, 16);
             this.label5.TabIndex = 6;
@@ -121,16 +121,16 @@
             // 
             // debugTbox
             // 
-            this.debugTbox.Location = new System.Drawing.Point(890, 174);
+            this.debugTbox.Location = new System.Drawing.Point(1004, 132);
             this.debugTbox.Multiline = true;
             this.debugTbox.Name = "debugTbox";
-            this.debugTbox.Size = new System.Drawing.Size(94, 293);
+            this.debugTbox.Size = new System.Drawing.Size(138, 424);
             this.debugTbox.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(582, 362);
+            this.label3.Location = new System.Drawing.Point(668, 422);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(32, 16);
             this.label3.TabIndex = 11;
@@ -138,29 +138,29 @@
             // 
             // nameTbox
             // 
-            this.nameTbox.Location = new System.Drawing.Point(633, 168);
+            this.nameTbox.Location = new System.Drawing.Point(713, 196);
             this.nameTbox.Name = "nameTbox";
-            this.nameTbox.Size = new System.Drawing.Size(169, 22);
+            this.nameTbox.Size = new System.Drawing.Size(191, 22);
             this.nameTbox.TabIndex = 12;
             // 
             // TypeTbox
             // 
-            this.TypeTbox.Location = new System.Drawing.Point(633, 190);
+            this.TypeTbox.Location = new System.Drawing.Point(713, 221);
             this.TypeTbox.Name = "TypeTbox";
-            this.TypeTbox.Size = new System.Drawing.Size(52, 22);
+            this.TypeTbox.Size = new System.Drawing.Size(91, 22);
             this.TypeTbox.TabIndex = 13;
             // 
             // levelTbox
             // 
-            this.levelTbox.Location = new System.Drawing.Point(633, 213);
+            this.levelTbox.Location = new System.Drawing.Point(713, 247);
             this.levelTbox.Name = "levelTbox";
-            this.levelTbox.Size = new System.Drawing.Size(52, 22);
+            this.levelTbox.Size = new System.Drawing.Size(91, 22);
             this.levelTbox.TabIndex = 14;
             // 
             // shinyCbox
             // 
             this.shinyCbox.AutoSize = true;
-            this.shinyCbox.Location = new System.Drawing.Point(692, 192);
+            this.shinyCbox.Location = new System.Drawing.Point(810, 250);
             this.shinyCbox.Name = "shinyCbox";
             this.shinyCbox.Size = new System.Drawing.Size(62, 20);
             this.shinyCbox.TabIndex = 15;
@@ -170,33 +170,19 @@
             // legendaryCbox
             // 
             this.legendaryCbox.AutoSize = true;
-            this.legendaryCbox.Location = new System.Drawing.Point(692, 212);
+            this.legendaryCbox.Location = new System.Drawing.Point(810, 224);
             this.legendaryCbox.Name = "legendaryCbox";
             this.legendaryCbox.Size = new System.Drawing.Size(94, 20);
             this.legendaryCbox.TabIndex = 16;
             this.legendaryCbox.Text = "Legendary";
             this.legendaryCbox.UseVisualStyleBackColor = true;
             // 
-            // hpUpDown
-            // 
-            this.hpUpDown.Location = new System.Drawing.Point(614, 427);
-            this.hpUpDown.Name = "hpUpDown";
-            this.hpUpDown.Size = new System.Drawing.Size(59, 22);
-            this.hpUpDown.TabIndex = 17;
-            // 
-            // expUpDown
-            // 
-            this.expUpDown.Location = new System.Drawing.Point(760, 427);
-            this.expUpDown.Name = "expUpDown";
-            this.expUpDown.Size = new System.Drawing.Size(52, 22);
-            this.expUpDown.TabIndex = 18;
-            // 
             // SaveBtn
             // 
             this.SaveBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
-            this.SaveBtn.Location = new System.Drawing.Point(247, 427);
+            this.SaveBtn.Location = new System.Drawing.Point(293, 497);
             this.SaveBtn.Name = "SaveBtn";
-            this.SaveBtn.Size = new System.Drawing.Size(108, 51);
+            this.SaveBtn.Size = new System.Drawing.Size(122, 59);
             this.SaveBtn.TabIndex = 19;
             this.SaveBtn.Text = "Save";
             this.SaveBtn.UseVisualStyleBackColor = false;
@@ -204,20 +190,58 @@
             // 
             // genUpDown
             // 
-            this.genUpDown.Location = new System.Drawing.Point(626, 360);
+            this.genUpDown.Location = new System.Drawing.Point(714, 420);
             this.genUpDown.Name = "genUpDown";
-            this.genUpDown.Size = new System.Drawing.Size(47, 22);
+            this.genUpDown.Size = new System.Drawing.Size(51, 22);
             this.genUpDown.TabIndex = 20;
+            // 
+            // hpTbox
+            // 
+            this.hpTbox.Location = new System.Drawing.Point(696, 497);
+            this.hpTbox.Name = "hpTbox";
+            this.hpTbox.Size = new System.Drawing.Size(69, 22);
+            this.hpTbox.TabIndex = 21;
+            // 
+            // expTbox
+            // 
+            this.expTbox.Location = new System.Drawing.Point(862, 500);
+            this.expTbox.Name = "expTbox";
+            this.expTbox.Size = new System.Drawing.Size(59, 22);
+            this.expTbox.TabIndex = 22;
+            // 
+            // attackComboBox
+            // 
+            this.attackComboBox.FormattingEnabled = true;
+            this.attackComboBox.Items.AddRange(new object[] {
+            "Attack",
+            "Defense",
+            "Sp_Attack",
+            "Sp_Defense"});
+            this.attackComboBox.Location = new System.Drawing.Point(783, 309);
+            this.attackComboBox.Name = "attackComboBox";
+            this.attackComboBox.Size = new System.Drawing.Size(121, 24);
+            this.attackComboBox.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(679, 312);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "AttackType";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1045, 542);
+            this.ClientSize = new System.Drawing.Size(1178, 648);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.attackComboBox);
+            this.Controls.Add(this.expTbox);
+            this.Controls.Add(this.hpTbox);
             this.Controls.Add(this.genUpDown);
             this.Controls.Add(this.SaveBtn);
-            this.Controls.Add(this.expUpDown);
-            this.Controls.Add(this.hpUpDown);
             this.Controls.Add(this.legendaryCbox);
             this.Controls.Add(this.shinyCbox);
             this.Controls.Add(this.levelTbox);
@@ -237,8 +261,6 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imagePbox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.hpUpDown)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.expUpDown)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.genUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -261,10 +283,12 @@
         private System.Windows.Forms.TextBox levelTbox;
         private System.Windows.Forms.CheckBox shinyCbox;
         private System.Windows.Forms.CheckBox legendaryCbox;
-        private System.Windows.Forms.NumericUpDown hpUpDown;
-        private System.Windows.Forms.NumericUpDown expUpDown;
         private System.Windows.Forms.Button SaveBtn;
         private System.Windows.Forms.NumericUpDown genUpDown;
+        private System.Windows.Forms.TextBox hpTbox;
+        private System.Windows.Forms.TextBox expTbox;
+        private System.Windows.Forms.ComboBox attackComboBox;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Pokedex 2.0/Pokedex 2.0/Form1.cs
+++ Pokedex 2.0/Pokedex 2.0/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -9,25 +10,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Pokedex_2._0
+namespace Pokedex
 {
-    enum attack
+    enum Attacks
     {
-        Special_Attack, Special_Defense, Attack, Defense
+        Attack, Sp_Attack, Defense, Sp_Defense
     }
-    struct pokemon
+    struct Pokemon
     {
-        string name;
-        string type;
-        int level;
-        attack attackType;
-        int hp;
-        int exp;
-        bool legendary;
-        bool shiny;
-        int gen;
+        public string Name;
+        public string Type;
+        public int Level;
+        public Attacks attackType;
+        public int HP;
+        public int Exp;
+        public bool Legendary;
+        public bool Shiny;
+        public int Gen;
     }
-
     public partial class Form1 : Form
     {
         public Form1()
@@ -41,31 +41,71 @@ namespace Pokedex_2._0
             {
                 StreamReader inFile = new StreamReader("Pokemon.txt");
                 string S = inFile.ReadToEnd();
+                Pokemon p = ReadPokemon(S);
+                ShowPokemon(p);
+                inFile.Close();
             }
+        }
+
+        private Pokemon ReadPokemon(string s)
+        {
+            Pokemon p = new Pokemon();
+            string[] fields = s.Split('|');
+            p.Name = fields[0];
+            p.Type = fields[1];
+            p.Level = int.Parse(fields[2]);
+            p.attackType = (Attacks)Enum.Parse(typeof(Attacks), fields[3]);
+            p.HP = int.Parse(fields[4]);
+            p.Exp = int.Parse(fields[5]);
+            if (fields[6] == "True")
+                p.Legendary = true;
+            else
+                p.Legendary = false;
+            if (fields[7] == "True")
+                p.Shiny = true;
+            else
+                p.Shiny = false;
+            p.Gen = int.Parse(fields[8]);
+
+            return p;
         }
 
         private void SaveBtn_Click(object sender, EventArgs e)
         {
-            debugTbox.Clear();
             debugTbox.Text += nameTbox.Text;
             debugTbox.Text += "|";
             debugTbox.Text += TypeTbox.Text;
             debugTbox.Text += "|";
             debugTbox.Text += levelTbox.Text;
             debugTbox.Text += "|";
-            debugTbox.Text += shinyCbox.Checked;
+            debugTbox.Text +=  attackComboBox.Text;
+            debugTbox.Text += "|";
+            debugTbox.Text += hpTbox.Text;
+            debugTbox.Text += "|";
+            debugTbox.Text += expTbox.Text;
             debugTbox.Text += "|";
             debugTbox.Text += legendaryCbox.Checked;
             debugTbox.Text += "|";
+            debugTbox.Text += shinyCbox.Checked;
+            debugTbox.Text += "|";
             debugTbox.Text += genUpDown.Value;
-            debugTbox.Text += "|";
-            debugTbox.Text += hpUpDown.Value;
-            debugTbox.Text += "|";
-            debugTbox.Text += expUpDown.Value;
 
-
+            StreamWriter outFile = new StreamWriter("Pokemon.txt");
+            outFile.Write(debugTbox.Text);
+            outFile.Close();
+        }
+        
+        private void ShowPokemon(Pokemon p)
+        {
+            nameTbox.Text = p.Name;
+            TypeTbox.Text = p.Type;
+            levelTbox.Text = p.Level.ToString();
+            attackComboBox.Text = p.attackType.ToString();
+            hpTbox.Text = p.HP.ToString();
+            expTbox.Text = p.Exp.ToString();
+            legendaryCbox.Checked = p.Legendary;
+            shinyCbox.Checked = p.Shiny;
+            genUpDown.Value = p.Gen;
         }
     }
-
-
 }
