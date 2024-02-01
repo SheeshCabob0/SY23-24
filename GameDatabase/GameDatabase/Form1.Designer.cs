3ddfbf54b92fb658a488a1c1d617fb21e1d54445
Derek W <dmwhi413@learner.dvusd.org>
2024-02-01 09:00:25 -0700

    Game Database 2/1/24
    Created and finished game database

---
+++ GameDatabase/GameDatabase/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace GameDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.creatorTbox = new System.Windows.Forms.TextBox();
+            this.revenueTbox = new System.Windows.Forms.TextBox();
+            this.characterTbox = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ratingCbox = new System.Windows.Forms.ComboBox();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.prevBtn = new System.Windows.Forms.Button();
+            this.nextBtn = new System.Windows.Forms.Button();
+            this.firstBtn = new System.Windows.Forms.Button();
+            this.lastBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGray;
+            this.label1.Font = new System.Drawing.Font("Verdana", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Game Database";
+            // 
+            // titleTbox
+            // 
+            this.titleTbox.BackColor = System.Drawing.Color.LightGray;
+            this.titleTbox.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleTbox.Location = new System.Drawing.Point(316, 101);
+            this.titleTbox.Margin = new System.Windows.Forms.Padding(4);
+            this.titleTbox.Name = "titleTbox";
+            this.titleTbox.Size = new System.Drawing.Size(485, 52);
+            this.titleTbox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.LightGray;
+            this.label2.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 108);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "        Title         ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.LightGray;
+            this.label3.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 188);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(315, 45);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "      Creator      ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.LightGray;
+            this.label4.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(41, 353);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(309, 45);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "     Revenue     ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.LightGray;
+            this.label5.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 431);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(320, 45);
+            this.label5.TabIndex = 5;
+            this.label5.Text = " Best Character ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.LightGray;
+            this.label6.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 272);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(320, 45);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "       Rating       ";
+            // 
+            // creatorTbox
+            // 
+            this.creatorTbox.BackColor = System.Drawing.Color.LightGray;
+            this.creatorTbox.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.creatorTbox.Location = new System.Drawing.Point(316, 181);
+            this.creatorTbox.Margin = new System.Windows.Forms.Padding(4);
+            this.creatorTbox.Name = "creatorTbox";
+            this.creatorTbox.Size = new System.Drawing.Size(485, 52);
+            this.creatorTbox.TabIndex = 7;
+            // 
+            // revenueTbox
+            // 
+            this.revenueTbox.BackColor = System.Drawing.Color.LightGray;
+            this.revenueTbox.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.revenueTbox.Location = new System.Drawing.Point(316, 346);
+            this.revenueTbox.Margin = new System.Windows.Forms.Padding(4);
+            this.revenueTbox.Name = "revenueTbox";
+            this.revenueTbox.Size = new System.Drawing.Size(485, 52);
+            this.revenueTbox.TabIndex = 9;
+            // 
+            // characterTbox
+            // 
+            this.characterTbox.BackColor = System.Drawing.Color.LightGray;
+            this.characterTbox.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.characterTbox.Location = new System.Drawing.Point(316, 424);
+            this.characterTbox.Margin = new System.Windows.Forms.Padding(4);
+            this.characterTbox.Name = "characterTbox";
+            this.characterTbox.Size = new System.Drawing.Size(485, 52);
+            this.characterTbox.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Location = new System.Drawing.Point(835, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 485);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ratingCbox
+            // 
+            this.ratingCbox.BackColor = System.Drawing.Color.LightGray;
+            this.ratingCbox.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ratingCbox.FormattingEnabled = true;
+            this.ratingCbox.Items.AddRange(new object[] {
+            "Everyone",
+            "Everyone_10_plus",
+            "Teen",
+            "Mature_17_plus",
+            "Adults_Only_18_plus",
+            "Rating_Pending",
+            "Rating_Pending_Likely_Mature_17_plus"});
+            this.ratingCbox.Location = new System.Drawing.Point(316, 264);
+            this.ratingCbox.Name = "ratingCbox";
+            this.ratingCbox.Size = new System.Drawing.Size(485, 53);
+            this.ratingCbox.TabIndex = 12;
+            // 
+            // newBtn
+            // 
+            this.newBtn.BackColor = System.Drawing.Color.LightGray;
+            this.newBtn.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newBtn.Location = new System.Drawing.Point(89, 504);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(180, 55);
+            this.newBtn.TabIndex = 14;
+            this.newBtn.Text = "New";
+            this.newBtn.UseVisualStyleBackColor = false;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // prevBtn
+            // 
+            this.prevBtn.BackColor = System.Drawing.Color.LightGray;
+            this.prevBtn.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prevBtn.Location = new System.Drawing.Point(353, 504);
+            this.prevBtn.Name = "prevBtn";
+            this.prevBtn.Size = new System.Drawing.Size(85, 55);
+            this.prevBtn.TabIndex = 15;
+            this.prevBtn.Text = "<";
+            this.prevBtn.UseVisualStyleBackColor = false;
+            this.prevBtn.Click += new System.EventHandler(this.prevBtn_Click);
+            // 
+            // nextBtn
+            // 
+            this.nextBtn.BackColor = System.Drawing.Color.LightGray;
+            this.nextBtn.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextBtn.Location = new System.Drawing.Point(443, 504);
+            this.nextBtn.Name = "nextBtn";
+            this.nextBtn.Size = new System.Drawing.Size(85, 55);
+            this.nextBtn.TabIndex = 16;
+            this.nextBtn.Text = ">";
+            this.nextBtn.UseVisualStyleBackColor = false;
+            this.nextBtn.Click += new System.EventHandler(this.nextBtn_Click);
+            // 
+            // firstBtn
+            // 
+            this.firstBtn.BackColor = System.Drawing.Color.LightGray;
+            this.firstBtn.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstBtn.Location = new System.Drawing.Point(551, 504);
+            this.firstBtn.Name = "firstBtn";
+            this.firstBtn.Size = new System.Drawing.Size(110, 55);
+            this.firstBtn.TabIndex = 17;
+            this.firstBtn.Text = "First";
+            this.firstBtn.UseVisualStyleBackColor = false;
+            this.firstBtn.Click += new System.EventHandler(this.firstBtn_Click);
+            // 
+            // lastBtn
+            // 
+            this.lastBtn.BackColor = System.Drawing.Color.LightGray;
+            this.lastBtn.Font = new System.Drawing.Font("Verdana", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastBtn.Location = new System.Drawing.Point(665, 504);
+            this.lastBtn.Name = "lastBtn";
+            this.lastBtn.Size = new System.Drawing.Size(110, 55);
+            this.lastBtn.TabIndex = 18;
+            this.lastBtn.Text = "Last";
+            this.lastBtn.UseVisualStyleBackColor = false;
+            this.lastBtn.Click += new System.EventHandler(this.lastBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(43)))), ((int)(((byte)(43)))));
+            this.ClientSize = new System.Drawing.Size(1359, 585);
+            this.Controls.Add(this.lastBtn);
+            this.Controls.Add(this.firstBtn);
+            this.Controls.Add(this.nextBtn);
+            this.Controls.Add(this.prevBtn);
+            this.Controls.Add(this.newBtn);
+            this.Controls.Add(this.ratingCbox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.characterTbox);
+            this.Controls.Add(this.revenueTbox);
+            this.Controls.Add(this.creatorTbox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.titleTbox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "GameDatabase";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox creatorTbox;
+        private System.Windows.Forms.TextBox revenueTbox;
+        private System.Windows.Forms.TextBox characterTbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox ratingCbox;
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button prevBtn;
+        private System.Windows.Forms.Button nextBtn;
+        private System.Windows.Forms.Button firstBtn;
+        private System.Windows.Forms.Button lastBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ GameDatabase/GameDatabase/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameDatabase
+{
+    enum Rating
+    {
+        Everyone, Everyone_10_plus, Teen, Mature_17_plus, Adults_Only_18_plus, Rating_Pending, Rating_Pending_Likely_Mature_17_plus
+    }
+    struct Game
+    {
+        public string title;
+        public string creator;
+        public Rating rating;
+        public string revenue;
+        public string character;
+        public string picture;
+    }
+
+    public partial class Form1 : Form
+    {
+        private Game[] games;
+        private int count;
+        private int current;
+        public string fileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            count = 0;
+            current = 0;
+            games = new Game[50];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Games.txt"))
+            {
+                StreamReader inFile = new StreamReader("Games.txt");
+                while (!inFile.EndOfStream)
+                {
+                    string S = inFile.ReadLine();
+                    Game g = ReadGame(S);
+                    games[count] = g;
+                    count++;
+                }
+                inFile.Close();
+                DisplayGame(games[0]);
+
+            }
+        }
+
+        private Game ReadGame(string s)
+        {
+            Game g = new Game();
+            string[] fields = s.Split('|');
+            g.title = fields[0];
+            g.creator = fields[1];
+            g.rating = (Rating)Enum.Parse(typeof(Rating), fields[2]);
+            g.revenue = fields[3];
+            g.character = fields[4];
+            g.picture = fields[5];
+
+            return g;
+        }
+
+        private void DisplayGame(Game g)
+        {
+            titleTbox.Text = g.title;
+            creatorTbox.Text = g.creator;
+            ratingCbox.Text = g.rating.ToString();
+            revenueTbox.Text = g.revenue.ToString();
+            characterTbox.Text = g.character;
+            pictureBox1.Load(g.picture);
+        }
+
+        private void Save()
+        {
+            string tmp = "";
+            tmp += titleTbox.Text;
+            tmp += "|";
+            tmp += creatorTbox.Text;
+            tmp += "|";
+            tmp += ratingCbox.Text;
+            tmp += "|";
+            tmp += revenueTbox.Text;
+            tmp += "|";
+            tmp += characterTbox.Text;
+            tmp += "|";
+            tmp += pictureBox1.ImageLocation;
+            games[current] = ReadGame(tmp);
+
+            StreamWriter outFile = new StreamWriter("Games.txt");
+            for (int i = 0; i < count; i++)
+            {
+                outFile.WriteLine(GameToString(games[i]));
+            }
+            outFile.Close();
+
+        }
+
+        private string GameToString(Game g)
+        {
+            string retVal = "";
+            retVal += g.title.ToString();
+            retVal += "|";
+            retVal += g.creator.ToString();
+            retVal += "|";
+            retVal += g.rating.ToString();
+            retVal += "|";
+            retVal += g.revenue.ToString();
+            retVal += "|";
+            retVal += g.character.ToString();
+            retVal += "|";
+            retVal += g.picture.ToString();
+            return retVal;
+        }
+
+        private void Clear()
+        {
+            titleTbox.Text = "";
+            creatorTbox.Text = "";
+            ratingCbox.Text = "";
+            revenueTbox.Text = "";
+            characterTbox.Text = "";
+            pictureBox1.Image = null;
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            current = count;
+            count++;
+            Clear();
+        }
+
+        private void prevBtn_Click(object sender, EventArgs e)
+        {
+            if (current > 0)
+            {
+                Save();
+                current--;
+                DisplayGame(games[current]);
+            }
+
+        }
+
+        private void nextBtn_Click(object sender, EventArgs e)
+        {
+            if (current < count - 1)
+            {
+                Save();
+                current++;
+                DisplayGame(games[current]);
+            }
+        }
+
+        private void firstBtn_Click(object sender, EventArgs e)
+        {
+            Save();
+            current = 0;
+            DisplayGame(games[current]);
+        }
+
+        private void lastBtn_Click(object sender, EventArgs e)
+        {
+            Save();
+            current = count - 1;
+            DisplayGame(games[current]);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Save();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.Load(openFileDialog1.FileName);
+            fileName = openFileDialog1.FileName;
+            debugTbox.Text = fileName;
+        }
+    }
+}
