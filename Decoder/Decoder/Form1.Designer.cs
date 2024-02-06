93f1b74142e8fede15cb6b0ad9a93ae4c137d96c
Derek W <dmwhi413@learner.dvusd.org>
2024-02-06 09:07:59 -0700

    Full Decoder Project Finished
    2/6/24

---
+++ Decoder/Decoder/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Decoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.encTbox = new System.Windows.Forms.TextBox();
+            this.decTbox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // encTbox
+            // 
+            this.encTbox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.encTbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encTbox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.encTbox.Location = new System.Drawing.Point(425, 65);
+            this.encTbox.Multiline = true;
+            this.encTbox.Name = "encTbox";
+            this.encTbox.Size = new System.Drawing.Size(287, 320);
+            this.encTbox.TabIndex = 0;
+            this.encTbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.encTbox_KeyPress);
+            // 
+            // decTbox
+            // 
+            this.decTbox.BackColor = System.Drawing.SystemColors.InfoText;
+            this.decTbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.decTbox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.decTbox.Location = new System.Drawing.Point(84, 65);
+            this.decTbox.Multiline = true;
+            this.decTbox.Name = "decTbox";
+            this.decTbox.Size = new System.Drawing.Size(287, 320);
+            this.decTbox.TabIndex = 1;
+            this.decTbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.decTbox_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(425, 391);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(287, 41);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Encoded";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(84, 391);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(287, 41);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Decoded";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(252, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(287, 41);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Magic Decoder";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.decTbox);
+            this.Controls.Add(this.encTbox);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Magic Decoder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox encTbox;
+        private System.Windows.Forms.TextBox decTbox;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Decoder/Decoder/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Decoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            encTbox.Text = "";
+            decTbox.Text = "";
+        }
+
+
+        private void decTbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case 'a':
+                    encTbox.Text += "@";
+                    break;
+                case 'b':
+                    encTbox.Text += "8";
+                    break;
+                case 'c':
+                    encTbox.Text += "[";
+                    break;
+                case 'd':
+                    encTbox.Text += "9";
+                    break;
+                case 'e':
+                    encTbox.Text += "#";
+                    break;
+                case 'f':
+                    encTbox.Text += "<";
+                    break;
+                case 'g':
+                    encTbox.Text += "6";
+                    break;
+                case 'h':
+                    encTbox.Text += "]";
+                    break;
+                case 'i':
+                    encTbox.Text += "(";
+                    break;
+                case 'j':
+                    encTbox.Text += "{";
+                    break;
+                case 'k':
+                    encTbox.Text += "}";
+                    break;
+                case 'l':
+                    encTbox.Text += "-";
+                    break;
+                case 'm':
+                    encTbox.Text += "$";
+                    break;
+                case 'n':
+                    encTbox.Text += "e";
+                    break;
+                case 'o':
+                    encTbox.Text += "%";
+                    break;
+                case 'p':
+                    encTbox.Text += "?";
+                    break;
+                case 'q':
+                    encTbox.Text += "u";
+                    break;
+                case 'r':
+                    encTbox.Text += "o";
+                    break;
+                case 's':
+                    encTbox.Text += "^";
+                    break;
+                case 't':
+                    encTbox.Text += "+";
+                    break;
+                case 'u':
+                    encTbox.Text += "n ";
+                    break;
+                case 'v':
+                    encTbox.Text += "w ";
+                    break;
+                case 'w':
+                    encTbox.Text += "v";
+                    break;
+                case 'x':
+                    encTbox.Text += "*";
+                    break;
+                case 'y':
+                    encTbox.Text += "\\";
+                    break;
+                case 'z':
+                    encTbox.Text += ">";
+                    break;
+
+                case ' ':
+                    encTbox.Text += "  ";
+                    break;
+
+                default:
+                    encTbox.Text += "";
+                    break;
+            }
+        }
+
+        private void encTbox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case '@':
+                    decTbox.Text += "a";
+                    break;
+                case '8':
+                    decTbox.Text += "b";
+                    break;
+                case '[':
+                    decTbox.Text += "c";
+                    break;
+                case '9':
+                    decTbox.Text += "d";
+                    break;
+                case '#':
+                    decTbox.Text += "e";
+                    break;
+                case '<':
+                    decTbox.Text += "f";
+                    break;
+                case '6':
+                    decTbox.Text += "g";
+                    break;
+                case ']':
+                    decTbox.Text += "h";
+                    break;
+                case '(':
+                    decTbox.Text += "i";
+                    break;
+                case '{':
+                    decTbox.Text += "j";
+                    break;
+                case '}':
+                    decTbox.Text += "k";
+                    break;
+                case '-':
+                    decTbox.Text += "l";
+                    break;
+                case '$':
+                    decTbox.Text += "m";
+                    break;
+                case 'e':
+                    decTbox.Text += "n";
+                    break;
+                case '%':
+                    decTbox.Text += "o";
+                    break;
+                case '?':
+                    decTbox.Text += "p";
+                    break;
+                case 'u':
+                    decTbox.Text += "q";
+                    break;
+                case 'o':
+                    decTbox.Text += "r";
+                    break;
+                case '^':
+                    decTbox.Text += "s";
+                    break;
+                case '+':
+                    decTbox.Text += "t";
+                    break;
+                case 'n':
+                    decTbox.Text += "u";
+                    break;
+                case 'w':
+                    decTbox.Text += "v";
+                    break;
+                case 'v':
+                    decTbox.Text += "w";
+                    break;
+                case '*':
+                    decTbox.Text += "x";
+                    break;
+                case '\\':
+                    decTbox.Text += "y";
+                    break;
+                case '>':
+                    decTbox.Text += "z";
+                    break;
+                case ' ':
+                    decTbox.Text += "  ";
+                    break;
+
+                default:
+                    decTbox.Text += "";
+                    break;
+            }
+        }
+    }
+}
