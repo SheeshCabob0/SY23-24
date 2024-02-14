4ac693d36983cab2d6e3b8d47d1421e8e4dd200a
Derek W <dmwhi413@learner.dvusd.org>
2024-02-14 09:12:03 -0700

    Planner, part 1 (2/14/24)
    Starts with one saved, and you can save one more, then read both...

---
+++ Planner/Planner/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Planner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.noteTbox = new System.Windows.Forms.TextBox();
+            this.dateTbox = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.priorityUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.doneCheckBox = new System.Windows.Forms.CheckBox();
+            this.categoryCombox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.readBtn = new System.Windows.Forms.Button();
+            this.debugTbox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.priorityUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(580, 37);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // noteTbox
+            // 
+            this.noteTbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noteTbox.Location = new System.Drawing.Point(120, 127);
+            this.noteTbox.Multiline = true;
+            this.noteTbox.Name = "noteTbox";
+            this.noteTbox.Size = new System.Drawing.Size(370, 409);
+            this.noteTbox.TabIndex = 1;
+            // 
+            // dateTbox
+            // 
+            this.dateTbox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(87)))), ((int)(((byte)(183)))));
+            this.dateTbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTbox.Location = new System.Drawing.Point(120, 37);
+            this.dateTbox.Name = "dateTbox";
+            this.dateTbox.Size = new System.Drawing.Size(370, 53);
+            this.dateTbox.TabIndex = 2;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveBtn.Location = new System.Drawing.Point(580, 256);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(262, 57);
+            this.saveBtn.TabIndex = 3;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // priorityUpDown
+            // 
+            this.priorityUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.priorityUpDown.Location = new System.Drawing.Point(693, 349);
+            this.priorityUpDown.Name = "priorityUpDown";
+            this.priorityUpDown.Size = new System.Drawing.Size(156, 34);
+            this.priorityUpDown.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(581, 354);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Priority";
+            // 
+            // doneCheckBox
+            // 
+            this.doneCheckBox.AutoSize = true;
+            this.doneCheckBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.doneCheckBox.Location = new System.Drawing.Point(586, 481);
+            this.doneCheckBox.Name = "doneCheckBox";
+            this.doneCheckBox.Size = new System.Drawing.Size(154, 33);
+            this.doneCheckBox.TabIndex = 8;
+            this.doneCheckBox.Text = "Completed";
+            this.doneCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // categoryCombox
+            // 
+            this.categoryCombox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categoryCombox.FormattingEnabled = true;
+            this.categoryCombox.Items.AddRange(new object[] {
+            "",
+            "Work",
+            "School",
+            "Entertainment",
+            "Goal",
+            "Improvement",
+            "Memory"});
+            this.categoryCombox.Location = new System.Drawing.Point(693, 418);
+            this.categoryCombox.Name = "categoryCombox";
+            this.categoryCombox.Size = new System.Drawing.Size(236, 37);
+            this.categoryCombox.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(577, 418);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(115, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Notes:";
+            // 
+            // readBtn
+            // 
+            this.readBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.readBtn.Location = new System.Drawing.Point(849, 256);
+            this.readBtn.Name = "readBtn";
+            this.readBtn.Size = new System.Drawing.Size(130, 57);
+            this.readBtn.TabIndex = 12;
+            this.readBtn.Text = "Read";
+            this.readBtn.UseVisualStyleBackColor = true;
+            this.readBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // debugTbox
+            // 
+            this.debugTbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debugTbox.Location = new System.Drawing.Point(849, 319);
+            this.debugTbox.Name = "debugTbox";
+            this.debugTbox.Size = new System.Drawing.Size(130, 34);
+            this.debugTbox.TabIndex = 13;
+            this.debugTbox.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(235)))), ((int)(((byte)(254)))));
+            this.ClientSize = new System.Drawing.Size(1002, 588);
+            this.Controls.Add(this.debugTbox);
+            this.Controls.Add(this.readBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.categoryCombox);
+            this.Controls.Add(this.doneCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.priorityUpDown);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.dateTbox);
+            this.Controls.Add(this.noteTbox);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.priorityUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.TextBox noteTbox;
+        private System.Windows.Forms.TextBox dateTbox;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.NumericUpDown priorityUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox doneCheckBox;
+        private System.Windows.Forms.ComboBox categoryCombox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button readBtn;
+        private System.Windows.Forms.TextBox debugTbox;
+    }
+}
+
---
+++ Planner/Planner/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Planner
+{
+    
+    enum Category { Work, School, Entertainment, Goal, Memory, }
+
+    public partial class Form1 : Form
+    {
+        private Day[] days;
+        private int count;
+        private int current;
+        string date;
+
+        public Form1()
+        {
+            InitializeComponent();
+            count = 0;
+            current = 0;
+            days = new Day[50];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Planner.txt"))
+            {
+                StreamReader infile = new StreamReader("Planner.txt");
+                while (!infile.EndOfStream)
+                {
+                    string S = infile.ReadLine();
+                    Day d = JsonSerializer.Deserialize<Day>(S);
+                    days[count] = d;
+                    count++;
+                }
+                infile.Close();
+                DisplayDate(days[0]);
+            }
+            else
+            {
+                Day d = new Day();
+                d.date = "12/14/24";
+                d.notes = "Current Date";
+                d.priority = 5;
+                d.category = Category.School;
+                d.done = false;
+                days[0] = d; 
+            }
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            date = monthCalendar1.SelectionRange.Start.ToShortDateString();
+            dateTbox.Text = date.ToString();
+        }
+
+        private void DisplayDate(Day d)
+        {
+            if (d != null)
+            {
+                dateTbox.Text = d.date;
+                noteTbox.Text = d.notes;
+                priorityUpDown.Value = d.priority;
+                categoryCombox.Text = d.category.ToString();
+                doneCheckBox.Checked = d.done;
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            //i < 1, instead of i < count
+            count++;
+            UpdatePlans();
+            StreamWriter outFile = new System.IO.StreamWriter("Planner.txt");
+            for (int i = 0; i < 2; i++)
+            {
+                string jsonString = JsonSerializer.Serialize(days[i]);
+                outFile.WriteLine(jsonString);
+            }
+            outFile.Close();
+        }
+
+        private void UpdatePlans()
+        {
+            //WARNING NOT SET TO CURRENT, NEED A WAY TO MAKE CURRENT!!
+            //Currently sets current to debugtextbox, which can be set while running the program
+            //current = int.Parse(debugTbox.Text.ToString());
+            days[1] = new Day();
+            Day d = days[1];
+
+            if (d != null)
+            {
+                d.date = dateTbox.Text.ToString();
+                d.notes = noteTbox.Text.ToString();
+                d.priority = int.Parse(priorityUpDown.Value.ToString());
+                d.category = (Category)Enum.Parse(typeof(Category), categoryCombox.Text.ToString());
+                    //Set "done" true or false
+                if (doneCheckBox.Checked)
+                    d.done = true;
+                else
+                    d.done = false;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int date = int.Parse(debugTbox.Text);
+
+            DisplayDate(days[date]);
+        }
+    }
+
+    class Day
+    {
+        public string date { get; set; }
+        public string notes { get; set; }
+        public int priority { get; set; }
+        public Category category { get; set; }
+        public bool done { get; set; }
+    }
+
+}
